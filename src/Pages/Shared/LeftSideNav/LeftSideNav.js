7a888fefc5105b68aafdb917f25b994eac2333dd
Tahsan <tahsanknmas14@gmail.com>
2023-02-15 18:52:26 +0600

    added module 61

--- src/Pages/Shared/LeftSideNav/LeftSideNav.js
+++ src/Pages/Shared/LeftSideNav/LeftSideNav.js
@@ -11,13 +11,12 @@ const LeftSideNav = () => {
     }, [])
     return (
         <div>
-            <h2>
-                LeftSideNav:{categories.length}
-            </h2>
+            <h4>All Category: {categories.length}</h4>
             <div>
                 {
                     categories.map(category => <p key={category.id}>
-                        <Link to={`/category/${category.id}`}>{category.name}</Link>
+                        <Link to={`/category/${category.id}`}>
+                            {category.name}</Link>
                     </p>)
                 }
             </div>
--- src/Pages/Shared/RightSideNav/RightSideNav.js
+++ src/Pages/Shared/RightSideNav/RightSideNav.js
@@ -1,15 +1,31 @@
-import React from 'react';
+import { GoogleAuthProvider } from 'firebase/auth';
+import React, { useContext } from 'react';
 import { ListGroup } from 'react-bootstrap';
 import Button from 'react-bootstrap/Button';
 import ButtonGroup from 'react-bootstrap/ButtonGroup';
 import { FaFacebook, FaGithub, FaGoogle, FaTwitch, FaTwitter, FaWhatsapp } from 'react-icons/fa';
+import { AuthContext } from '../../../Contexts/AuthProvider/AuthProvider';
 import BrandCarousel from '../BandCarousel/BrandCarousel';
 
 const RightSideNav = () => {
+
+    const { providerLogin } = useContext(AuthContext);
+
+    const googleProvider = new GoogleAuthProvider()
+
+    const handleGoogleSignIn = () => {
+        providerLogin(googleProvider)
+            .then(result => {
+                const user = result.user;
+                console.log(user);
+            })
+            .catch(error => console.error(error))
+    }
+
     return (
         <div>
             <ButtonGroup vertical>
-                <Button className='mb-2' variant="outline-primary"><FaGoogle /> Login with Google</Button>
+                <Button onClick={handleGoogleSignIn} className='mb-2' variant="outline-primary"><FaGoogle /> Login with Google</Button>
                 <Button variant="outline-dark"><FaGithub /> Login with Github</Button>
             </ButtonGroup>
             <div className='mt-4'>
